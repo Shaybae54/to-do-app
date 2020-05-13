7d42ff233e606fb956b55a7a2c5221b8ecbb1be1
Shaybae54 <54779145+Shaybae54@users.noreply.github.com>
2020-05-13 18:18:34 -0400

    assignment updated again

--- app.js
+++ app.js
@@ -20,18 +20,18 @@ function onReady() {
 
     //add delete button
     let deleteButton = document.createElement('button');
-    deleteButton.textContent = "Delete";
+    deleteButton.textContent = "Delete"
 
     deleteButton.addEventListener('click', function(event) {
-      toDoList.removeChild(this.parentElement);
-    });
+      toDolist.removeChild(this.parentElement);
+    })
 
     //set the title
     newLi.textContent = title;
 
     //attach the checkbox to the li
     newLi.appendChild(checkbox);
-    newLi.appendChild(deleteButton);
+    newLi.appendChild(deletButton);
 
     //attach the li to the ul
     toDoList.appendChild(newLi);
@@ -43,5 +43,6 @@ function onReady() {
 }
 
 window.onload = function() {
+  alert("The window is loaded!");
   onReady();
 };
